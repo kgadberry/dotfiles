ca4895aeed7165f9b7310252f738af58e9f5c103
Kaye Gadberry <1781520+kgadberry@users.noreply.github.com>
2024-09-14 20:06:54 -0700

    updating

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -1,56 +1,7 @@
-{ config, pkgs, lib, ... }: {
-
-    options = {
-
-        passwordHash = lib.mkOption {
-            type = lib.types.nullOr lib.types.str;
-            description = "Password created with sha512.";
-            default = null;
-        };
-
-    };
-
-    config = lib.mkIf (pkgs.stdenv.isLinux) {
-
-        # Allows us to declaritively set password
-        users.mutableUsers = false;
-
-        # Define a user account. Don't forget to set a password with ‘passwd’.
-        users.users.${config.user} = {
-
-            # Create a home directory for human user
-            isNormalUser = true;
-
-            # Automatically create a password to start
-            hashedPassword = config.passwordHash;
-
-            extraGroups = [
-                "wheel" # Sudo privileges
-            ];
-
-        };
-
-        home-manager.users.${config.user}.xdg = {
-
-            # Allow Nix to manage the default applications list
-            mimeApps.enable = true;
-
-            # Set directories for application defaults
-            userDirs = {
-                enable = true;
-                createDirectories = true;
-                documents = "$HOME/Documents";
-                download = "$HOME/Downloads";
-                music = "$HOME/Media/Music";
-                pictures = "$HOME/Media/Images";
-                videos = "$HOME/Media/Videos";
-                desktop = "$HOME/Other/Desktop";
-                publicShare = "$HOME/Other/Public";
-                templates = "$HOME/Other/Templates";
-                extraConfig = { XDG_DEV_DIR = "$HOME/Code"; };
-            };
-        };
-
-    };
-
+{ ... }:
+{
+    imports = [ 
+        ./user.nix
+        ./networking.nix
+    ];
 }
